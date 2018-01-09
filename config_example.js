012d21eec9a1b4424319ee3f186bb0655f3a7b8d
Michael J. Flynn <flynnmj@devnoise.com>
2018-01-08 22:01:41 -0500

    Refactoring the Circle drawing.

---
+++ config_example.js
@@ -0,0 +1,15 @@
+let config = {
+  mapboxAccessToken: 'your.mapbox.access.token',
+  zones: [
+    {
+      lat: 40.211785,
+      lng: -75.549030,
+      radius: 20000,
+      color: '#F44336',
+      text: '#MontCo/ChesCo'
+    }
+  ]
+
+};
+
+export default config;
--- index.js
+++ index.js
@@ -1,85 +1,83 @@
-var mymap = L.map('mapid').setView([40.13321276, -75.44876289], 11);
+import config from './config';
 
-L.tileLayer(
-  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-  {
-    attribution:
-      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken:
-      'your.mapbox.access.token'
+let geoLayers;
+
+let mymap = L.map('mapid').setView([
+  40.13321276, -75.44876289
+], 11);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: config.mapboxAccessToken
+}).addTo(mymap);
+
+// let zones = [
+//   {
+//     lat: 40.211785,
+//     lng: -75.549030,
+//     radius: 5000,
+//     text: '#1 Royerford/Limerick'
+//   }, {
+//     lat: 40.2020175,
+//     lng: -75.444939,
+//     radius: 8000,
+//     text: '#2 Collegville/Trappe'
+//   }, {
+//     lat: 40.135492,
+//     lng: -75.526528,
+//     radius: 5000,
+//     text: '#3 Phoenixville'
+//   }, {
+//     lat: 40.1421369,
+//     lng: -75.4261191,
+//     radius: 4000,
+//     text: '#4 Oaks/Audubon'
+//   }, {
+//     lat: 40.0996742,
+//     lng: -75.4059501,
+//     radius: 5000,
+//     text: '#6 King of Prussia/Valley Forge'
+//   }, {
+//     lat: 40.0531847,
+//     lng: -75.518217,
+//     radius: 6000,
+//     text: '#7 Chesterbrook/Malvern/Paoli'
+//   }, {
+//     lat: 40.088199,
+//     lng: -75.270557,
+//     radius: 4000,
+//     text: '#8 Conshohockon/Plymouth Meeting'
+//   }
+// ];
+
+let removeBreweries = function() {
+  if (geoLayers) {
+    mymap.removeLayer(geoLayers);
   }
-).addTo(mymap);
+};
 
-let text = 'Raid Channel Here';
+let zoneSort = function(a, b) {
+  return b.radius - a.radius;
+};
 
-// #1  !sight 40.211785 -75.549030 5
-L.circle(L.latLng(40.212, -75.549), 5000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#1 Royerford/Limerick'))
-  .addTo(mymap);
+let drawZones = function(zones) {
+  removeBreweries();
 
-// #2  !setup 40.2020175,-75.444939
-L.circle(L.latLng(40.202, -75.445), 8000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#2 Collegville/Trappe'))
-  .addTo(mymap);
+  geoLayers = L.layerGroup();
 
-// #3  !setup 40.135492,-75.526528
-L.circle(L.latLng(40.135, -75.526), 5000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#3 Phoenixville'))
-  .addTo(mymap);
+  zones.sort(zoneSort).forEach(zone => {
+    let newLayer = L.circle(L.latLng(zone.lat, zone.lng), zone.radius, {
+      opacity: 1,
+      weight: 1,
+      fillOpacity: 0.25,
+      color: zone.color
+    }).bindPopup(String(zone.text)).addTo(mymap);
+    geoLayers.addLayer(newLayer);
+  });
 
-// #4  !sight 40.1421369 -75.4261191 4
-L.circle(L.latLng(40.142, -75.426), 4000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#4 Oaks/Audubon'))
-  .addTo(mymap);
+  mymap.addLayer(geoLayers);
+}
 
-// #6  !setup 40.0996742,-75.4059501
-L.circle(L.latLng(40.099, -75.406), 5000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#6 King of Prussia/Valley Forge'))
-  .addTo(mymap);
-
-// #7  !setup 40.0531847,-75.518217
-L.circle(L.latLng(40.053, -75.518), 6000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#7 Chesterbrook/Malvern/Paoli'))
-  .addTo(mymap);
-
-// #8  !setup 40.088199,-75.270557
-L.circle(L.latLng(40.088, -75.271), 4000, {
-  opacity: 1,
-  weight: 1,
-  fillOpacity: 0.25,
-  color: '#0047AB'
-})
-  .bindPopup(String('#8 Conshohockon/Plymouth Meeting'))
-  .addTo(mymap);
+drawZones(config.zones);
